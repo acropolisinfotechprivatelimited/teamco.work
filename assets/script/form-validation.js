0f4283c54a920a006829a86e479f139d8de3da53
pranaysharma <pranaysharma995@gmail.com>
2019-07-05 14:38:19 +0530

    Update form validation page

--- assets/script/form-validation.js
+++ assets/script/form-validation.js
@@ -11,77 +11,156 @@ jQuery(document).ready(function()
        console.log("error removed  at"+element);
    }
    
-   $(".acropolis_mobile").on("change",function()
+   $(".desktop_mobile").on("change",function()
    {
        number_validation();
    })
-   $(".acropolis_email").on("change",function()
+    $(".mobile_mobile").on("change",function()
+   {
+       number_validation_mobile();
+   })
+    $(".inquire_mobile").on("change",function()
+   {
+       number_validation_inquire();
+   })
+   $(".desktop_email").on("change",function()
    {
        email_validation();
+   })
+     $(".inquire_email").on("change",function()
+   {
+       email_validation_inquire();
+   })
+     $(".mobile_email").on("change",function()
+   {
+       email_validation_mobile();
    })
    //validation in contac form
    
    var number_validation=function()
    {
-       let mobile_number=jQuery(".acropolis_mobile").val();
+       let mobile_number=jQuery(".desktop_mobile").val();
        console.log(mobile_number);
        if(mobile_number.length <= 0 || mobile_number.length > 12)
        {
-           show_error(jQuery(".acropolis_mobile"));
+           show_error(jQuery(".desktop_mobile"));
            return false;
            
            
        }
        else if(mobile_number.length > 0 && mobile_number.length < 10)
        {
-            show_error(jQuery(".acropolis_mobile"));
+            show_error(jQuery(".desktop_mobile"));
            return false;
        }
        else
        {
-           hide_error(jQuery(".acropolis_mobile"));
+           hide_error(jQuery(".desktop_mobile"));
            return true;
              
        }
    }
-   var password_validation=function()
+     var number_validation_inquire=function()
    {
-       let password=jQuery(".acropolis_password").val();
-       console.log(password);
-       if(password.length <= 0 || password.length > 12)
+       let mobile_number=jQuery(".inquire_mobile").val();
+       console.log(mobile_number);
+       if(mobile_number.length <= 0 || mobile_number.length > 12)
        {
-           show_error(jQuery(".acropolis_password"));
+           show_error(jQuery(".inquire_mobile"));
            return false;
            
            
        }
-       else if(password.length > 0 && password.length < 8)
+       else if(mobile_number.length > 0 && mobile_number.length < 10)
        {
-            show_error(jQuery(".acropolis_password"));
+            show_error(jQuery(".inquire_mobile"));
            return false;
        }
        else
        {
-           hide_error(jQuery(".acropolis_password"));
+           hide_error(jQuery(".inquire_mobile"));
+           return true;
+             
+       }
+   }
+       var number_validation_mobile=function()
+   {
+       let mobile_number=jQuery(".mobile_mobile").val();
+       console.log(mobile_number);
+       if(mobile_number.length <= 0 || mobile_number.length > 12)
+       {
+           show_error(jQuery(".mobile_mobile"));
+           return false;
+           
+           
+       }
+       else if(mobile_number.length > 0 && mobile_number.length < 10)
+       {
+            show_error(jQuery(".mobile_mobile"));
+           return false;
+       }
+       else
+       {
+           hide_error(jQuery(".mobile_mobile"));
            return true;
              
        }
    }
    var name_validation=function()
    {
-       let first_name=jQuery(".acropolis_firstname").val();
+       let first_name=jQuery(".desktop_firstname").val();
        //let last_name=jQuery(".acropolis_lastname").val();
        console.log(first_name);
        if(first_name.length <=0)
        {
-             show_error(jQuery(".acropolis_firstname"));
+             show_error(jQuery(".desktop_firstname"));
                //show_error(jQuery(".acropolis_lastname"));
            return false;
            
        }
        else
        {
-            hide_error(jQuery(".acropolis_firstname"));
+            hide_error(jQuery(".desktop_firstname"));
+           //hide_error(jQuery(".acropolis_lastname"));
+           return true;
+            
+       }
+   }
+     var name_validation_inquire=function()
+   {
+       let first_name=jQuery(".inquire_firstname").val();
+       //let last_name=jQuery(".acropolis_lastname").val();
+       console.log(first_name);
+       if(first_name.length <=0)
+       {
+             show_error(jQuery(".inquire_firstname"));
+               //show_error(jQuery(".acropolis_lastname"));
+           return false;
+           
+       }
+       else
+       {
+            hide_error(jQuery(".inquire_firstname"));
+           //hide_error(jQuery(".acropolis_lastname"));
+           return true;
+            
+       }
+   }
+       var name_validation_mobile=function()
+   {
+       let first_name=jQuery(".mobile_firstname").val();
+       //let last_name=jQuery(".acropolis_lastname").val();
+       console.log(first_name);
+       if(first_name.length <=0)
+       {
+             show_error(jQuery(".mobile_firstname"));
+               //show_error(jQuery(".acropolis_lastname"));
+           return false;
+           
+       }
+       else
+       {
+            hide_error(jQuery(".mobile_firstname"));
            //hide_error(jQuery(".acropolis_lastname"));
            return true;
             
@@ -89,21 +168,65 @@ jQuery(document).ready(function()
    }
    var email_validation=function()
    {
-       let email=jQuery(".acropolis_email").val();
+       let email=jQuery(".desktop_email").val();
        console.log(email);
        let dot=email.includes(".");
        let at_the_rate=email.includes("@");
        console.log(dot);
        if(email.length > 0 && dot && at_the_rate )
        {
-           hide_error(jQuery(".acropolis_email"));
+           hide_error(jQuery(".desktop_email"));
            return true;
           
          
        }
        else
        {
-             show_error(jQuery(".acropolis_email"));
+             show_error(jQuery(".desktop_email"));
+           return false;
+
+             
+       }
+   }
+      var email_validation_inquire=function()
+   {
+       let email=jQuery(".inquire_email").val();
+       console.log(email);
+       let dot=email.includes(".");
+       let at_the_rate=email.includes("@");
+       console.log(dot);
+       if(email.length > 0 && dot && at_the_rate )
+       {
+           hide_error(jQuery(".inquire_email"));
+           return true;
+          
+         
+       }
+       else
+       {
+             show_error(jQuery(".inquire_email"));
+           return false;
+
+             
+       }
+   }
+         var email_validation_mobile=function()
+   {
+       let email=jQuery(".mobile_email").val();
+       console.log(email);
+       let dot=email.includes(".");
+       let at_the_rate=email.includes("@");
+       console.log(dot);
+       if(email.length > 0 && dot && at_the_rate )
+       {
+           hide_error(jQuery(".mobile_email"));
+           return true;
+          
+         
+       }
+       else
+       {
+             show_error(jQuery(".mobile_email"));
            return false;
 
              
@@ -124,10 +247,42 @@ jQuery(document).ready(function()
            return false;
        }
    }
+    var form_validation_inquire=function()
+   {   
+       let number_validation_status=number_validation_inquire();
+       //let password_validation_status=password_validation();
+       let name_validation_status=name_validation_inquire();
+       let email_validation_status=email_validation_inquire();
+       if(number_validation_status && name_validation_status && email_validation_status)
+       {
+           return true;
+       }
+       else
+       {
+           return false;
+       }
+   }
+     var form_validation_mobile=function()
+   {   
+       let number_validation_status=number_validation_mobile();
+       //let password_validation_status=password_validation();
+       let name_validation_status=name_validation_mobile();
+       let email_validation_status=email_validation_mobile();
+       if(number_validation_status && name_validation_status && email_validation_status)
+       {
+           return true;
+       }
+       else
+       {
+           return false;
+       }
+   }
+   
+   
    
      //disable character keys 
 
-   jQuery('.acropolis_mobile').keypress(function(key) {
+   jQuery('[name="mobile"]').keypress(function(key) {
        if(key.charCode == 32)
        {
            return true;
@@ -146,7 +301,7 @@ jQuery(document).ready(function()
        
                   //disable number keys 
        
-   jQuery('.acropolis_firstname').keypress(function(key) {
+   jQuery('[name="name"]').keypress(function(key) {
        if(key.charCode == 32)
            {
                return true;
@@ -165,7 +320,7 @@ jQuery(document).ready(function()
        
        });
    
-   jQuery("form").on("submit",function(e)
+   jQuery(".contact-form").on("submit",function(e)
       {
                     e.preventDefault();
           if(form_validation())
@@ -178,6 +333,77 @@ jQuery(document).ready(function()
            $.ajax({
                type:"post",
                url:"http://teamcowork.acropolisglobal.com/sendMail.php",
+               data:fd,
+               contentType: false,
+               cache: false,
+               processData:false,
+               success:function(data)
+               {
+                   console.log(data);
+                    $(".loading-container.page-loader").hide();
+                    $("#myModal").modal('show');
+                   $(".name").val(" "),
+                   $(".mobile").val(" "),
+                   $(".email").val(" ")
+               }
+              
+           })
+          }
+          else
+          {
+            console.log("Fix Error");
+          }
+          
+      });
+     jQuery(".contact-form-mobile").on("submit",function(e)
+      {
+                    e.preventDefault();
+          if(form_validation_mobile())
+          {
+
+           console.log("Welcome");
+           $(".loading-container.page-loader").show();
+
+            var fd=new FormData(this);
+           $.ajax({
+               type:"post",
+               url:"http://teamcowork.acropolisglobal.com/sendMail.php",
+               data:fd,
+               contentType: false,
+               cache: false,
+               processData:false,
+               success:function(data)
+               {
+                   console.log(data);
+                    $(".loading-container.page-loader").hide();
+                    $("#myModal").modal('show');
+                   $(".name").val(" "),
+                   $(".mobile").val(" "),
+                   $(".email").val(" ")
+               }
+              
+           })
+          }
+          else
+          {
+            console.log("Fix Error");
+          }
+          
+      });
+    
+     jQuery(".inquire-form").on("submit",function(e)
+      {
+                    e.preventDefault();
+          if(form_validation_inquire())
+          {
+
+           console.log("Welcome");
+           $(".loading-container.page-loader").show();
+
+            var fd=new FormData(this);
+           $.ajax({
+               type:"post",
+               url:"http://teamcowork.acropolisglobal.com/sendinquire.php",
                data:fd,
                contentType: false,
                cache: false,
